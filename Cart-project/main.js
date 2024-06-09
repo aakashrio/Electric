b7990f9b7a9b6ba4a6bbb77fe89124eb4c36a10b
Aakash <aakashjothi20@gmail.com>
2024-06-09 16:06:20 +0530

    first commit

---
+++ Cart-project/main.js
@@ -0,0 +1,515 @@
+// let whlist = new Set (JSON.parse(localStorage.getItem('whlist')) || []);
+// let cart = new Set (JSON.parse(localStorage.getItem('cart')) || []);
+
+let gta6 = {
+  id: "gta6",
+  name: "Grand Theft Auto Vl",
+  des: "Grand Theft Auto VI is an upcoming action-adventure game in development by Rockstar Games. It is due to be the eighth main Grand Theft Auto game, following Grand Theft Auto V, and the sixteenth entry overall.",
+  price: "4000",
+  image: "data/images/poster/gta6.jpeg",
+  category: "AAA",
+};
+
+let god = {
+  id: "god",
+  name: "God Of War : Ragnarok",
+  des: "God of War Ragnarök is an action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment.",
+  price: "2340",
+  image: "data/images/poster/god.jpg",
+  category: "Action",
+};
+
+let rdr2 = {
+  id: "rdr2",
+  name: "Red Dead Redemption 2",
+  des: "Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games. The game is the third entry in the Red Dead series and a prequel to the 2010 game Red Dead Redemption.",
+  price: "3460",
+  image: "data/images/poster/rdr2.jpeg",
+  category: "AAA",
+};
+
+let ark2 = {
+  id: "ark2",
+  name: "ARK 2",
+  des: "Survive the past. Tame the future. Suddenly awakened on a primal world filled with dinosaurs and humans struggling for dominance, you must team-up with legendary heroes to confront powerful dark forces. Saddle up, and join the definitive next-gen survival experience with ARK 2",
+  price: "5340",
+  image: "data/images/poster/ark2.jpg",
+  category: "Survival",
+};
+
+let ets2 = {
+  id: "ets2",
+  name: "Euro Truck Simulator 2",
+  des: "Euro Truck Simulator 2 gives you the chance to become a real truck driver from the comfort of your home! Featuring licensed trucks with countless customization options and advanced driving physics, the game delivers an unparalleled driving experience which has put it in the spot of the most popular truck driving simulator on the market",
+  price: "800",
+  image: "data/images/poster/ets2.jpg",
+  category: "Simulation",
+};
+
+let gta5 = {
+  id: "gta5",
+  name: "Grand Theft Auto V",
+  des: "Grand Theft Auto V is an action-adventure game played from either a third-person or first-person perspective. Players complete missions—linear scenarios with set objectives—to progress through the story. Outside of the missions, players may freely roam the open world.",
+  price: "1200",
+  image: "data/images/poster/gta5.jpeg",
+  category: "AAA",
+};
+
+let fh5 = {
+  id: "fh5",
+  name: "Forza Horizon 5",
+  des: "Forza Horizon 5 is a 2021 racing video game developed by Playground Games and published by Xbox Game Studios. It is the fifth Forza Horizon title and twelfth main instalment in the Forza series. The game is set in a fictionalised representation of Mexico",
+  price: "3340",
+  image: "data/images/poster/fh5.jpg",
+  category: "Raceing",
+};
+
+let aw2 = {
+  id: "aw2",
+  name: "Alan Wake 2",
+  des: "Saga Anderson arrives to investigate ritualistic murders in a small town. Alan Wake pens a dark story to shape the reality around him. These two heroes are somehow connected. Can they become the heroes they need to be?",
+  price: "4640",
+  image: "data/images/poster/aw2.jpg",
+  category: "Survival horror",
+};
+
+let sf = {
+  id: "sf",
+  name: "Street Fighter",
+  des: "In the original, players have to fight their way through a martial arts competition that spans five countries and 10 unique opponents who all use different fighting styles",
+  price: "340",
+  image: "data/images/poster/sf.jpg",
+  category: "Action",
+};
+
+let smw = {
+  id: "smw",
+  name: "Super Mario World",
+  des: "In this game, Mario is out to save Princess Toadstool from Bowser and his Koopalings, and every level is filled with nostalgia",
+  price: "1340",
+  image: "data/images/poster/smw.jpeg",
+  category: "Action",
+};
+
+let gtavc = {
+  id: "gtavc",
+  name: "Grand Theft Auto: Vice City",
+  des: "The game's plot is based on multiple real-world people and events in Miami such as Cubans, Haitians, and biker gangs, the 1980s crack epidemic, the Mafioso drug lords of Miami, and the dominance of glam metal. The game was also influenced by the films and television of the era, most notably Scarface and Miami Vice.",
+  price: "540",
+  image: "data/images/poster/gtavc.jpg",
+  category: "AAA",
+};
+
+let nfsmw = {
+  id: "nfsmw",
+  name: "Need for Speed: MW(2005)",
+  des: "Need for Speed: Most Wanted is an open world racing game developed by Criterion Games and published by Electronic Arts",
+  price: "230",
+  image: "data/images/poster/nfsmw.jpg",
+  category: "Raceing",
+};
+
+let fs = {
+  id: "fs",
+  name: "Farming Simulator 22",
+  des: "Enjoy a huge variety of farming operations focusing on agriculture, animal husbandry and forestry. Operate real machines from John Deere, CLAAS, Case IH, DEUTZ-FAHR, New Holland, Fendt, Massey Ferguson, Valtra and many others in three diverse American and European environments. Now with the exciting addition of seasonal cycles and production chains!",
+  price: "840",
+  image: "data/images/poster/fs22.jpg",
+  category: "Simulation",
+};
+
+let sk2 = {
+  id: "sk2",
+  name: "city:skyline 2",
+  des: "Cities: Skylines II is a 2023 city-building game developed by Colossal Order and published by Paradox Interactive. The game is a sequel to 2015's Cities: Skylines and expands upon many of its simulation factors such as simulated city and population sizes with improved traffic AI and management schemes",
+  price: "2400",
+  image: "data/images/poster/sk2.jpg",
+  category: "Simulation",
+};
+
+let gtasa = {
+  id: "gtasa",
+  name: "Grand Theft Auto: San Andreas",
+  des: "Grand Theft Auto: San Andreas is a 2004 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the fifth main entry in the Grand Theft Auto series, following 2002's Grand Theft Auto: Vice City, and the seventh installment overall",
+  price: "900",
+  image: "data/images/poster/gtasa.jpg",
+  category: "AAA",
+};
+
+let c22 = {
+  id: "c22",
+  name: "Cricket 24",
+  des: "Cricket 22 is a 2021 cricket video game developed by Big Ant Studios and published by Nacon. It is the official video game of the 2021–22 Ashes series of cricket matches, and the sequel to the 2019 game Cricket 19",
+  price: "999",
+  image: "data/images/poster/c22.jpg",
+  category: "Sports",
+};
+
+let er = {
+  id: "er",
+  name: "Elden Ring",
+  des: "Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between,In the Lands Between ruled by Queen Marika the Eternal, the Elden Ring, the source of the Erdtree, has been shattered",
+  price: "3700",
+  image: "data/images/poster/er.jpg",
+  category: "AAA",
+};
+
+let bg3 = {
+  id: "bg3",
+  name: "Baldur's Gate 3",
+  des: "Baldur's Gate 3 is a 2023 role-playing video game developed and published by Belgian game developer Larian Studios. The game is the third main installment in the Baldur's Gate series, based on the tabletop fantasy role-playing system of Dungeons & Dragons",
+  price: "2500",
+  image: "data/images/poster/bg3.jpg",
+  category: "AAA",
+};
+// name:"God Of War : Ragnarok",
+//     des:"God of War Ragnarök is an action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment.",
+//     price:"2340",
+//     image:"data/images/poster/god.jpg",
+//     category:"Action"
+
+// data for  function /gams info
+let games = [
+  gta6,
+  god,
+  rdr2,
+  ark2,
+  ets2,
+  gta5,
+  fh5,
+  aw2,
+  sf,
+  smw,
+  gtavc,
+  nfsmw,
+  fs,
+  sk2,
+  gtasa,
+  c22,
+  er,
+  bg3,
+];
+
+// let whi=[god,rdr2]
+
+//contact form function
+
+function cot() {
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+
+  if (name == "" || email == "") {
+    alert("Enter the vaild data");
+  } else {
+    alert(
+      "Hello" +
+        " " +
+        name +
+        " " +
+        "your Response was submited" +
+        "\n" +
+        "We sortly contact with Your" +
+        " " +
+        email
+    );
+  }
+}
+
+//accoint login functiom
+function acc() {
+  v;
+  alert("This serives currently unavilable");
+}
+
+//auto genrateing function for explore tap/web page
+
+function explore() {
+  // for loop creating card how many elements present in data array
+  let at = 0;
+  let t = 0;
+  for (i = 0; i < games.length; i++) {
+    //creating display card for games
+    const a = document.createElement("div");
+    document.getElementById("main").append(a);
+    a.className = "contentdisplay";
+    a.id = "g" + at;
+
+    //creating for insert images on card
+    const b = document.createElement("img");
+    a.append(b);
+    b.className = "displayimage";
+    b.src = games[i].image;
+
+    //creating for displying names
+
+    const c1 = document.createElement("span");
+    a.append(c1);
+    c1.className = "displaybase";
+    c1.innerHTML = "Base Game";
+
+    const c = document.createElement("span");
+    a.append(c);
+    c.className = "displayname";
+    c.innerHTML = games[i].name;
+
+    //creating for display prices
+    const d = document.createElement("span");
+    a.append(d);
+    d.className = "displayprice";
+    d.innerHTML =
+      games[i].price +
+      " " +
+      '<i class="fa-solid fa-indian-rupee-sign fa-sm"></i>';
+
+    //creating cart button container
+
+    const e = document.createElement("div");
+    a.append(e);
+    e.className = "buttoncontainer";
+    e.id = "b" + t;
+    t = t + 1;
+
+    //creating cart button
+
+    var cartadd = function () {
+      let h2 = this.parentElement.id;
+      let res = h2.slice(1);
+      alert(games[res].name + " " + "was added in your Cart");
+      let cart = new Set(JSON.parse(localStorage.getItem("cart")) || []);
+      cart.add(games[res]);
+      localStorage.setItem("cart", JSON.stringify(Array.from(cart)));
+    };
+
+    const f = document.createElement("button");
+    e.append(f);
+    f.className = "cartbutton";
+    f.innerHTML = '<i class="fa-solid fa-cart-shopping">';
+    f.addEventListener("click", cartadd);
+
+    var whiashdd = function () {
+      let h2 = this.parentElement.id;
+      let res = h2.slice(1);
+      alert(games[res].name + " " + "was added in your Whishlist");
+      let whlist = new Set(JSON.parse(localStorage.getItem("whlist")) || []);
+      whlist.add(games[res]);
+      localStorage.setItem("whlist", JSON.stringify(Array.from(whlist)));
+    };
+
+    //creating whishlist buttton
+
+    const g = document.createElement("button");
+    e.append(g);
+    g.className = "whishbutton";
+    g.innerHTML = '<i class="fa-regular fa-bookmark">';
+    g.addEventListener("click", whiashdd);
+    at = at + 1;
+  }
+}
+
+function whis() {
+  let whi = JSON.parse(localStorage.getItem("whlist")) || [];
+  if (whi[0] == null) {
+    whi = whi.slice(1);
+  } else {
+    whi = whi;
+  }
+  let temp = whi.filter(
+    (obj, index, self) => index === self.findIndex((o) => o.name === obj.name)
+  );
+  function het() {
+    let val = temp;
+    let t = 0;
+    if (val.length == 0) {
+      const an1 = document.createElement("div");
+      document.getElementById("main").append(an1);
+      an1.className = "notcontent";
+
+      const bn1 = document.createElement("img");
+      an1.append(bn1);
+      bn1.className = "notimage";
+      bn1.src = "data/images/empty_whish.png";
+    } else {
+      at = 0;
+      for (i = 0; i < val.length; i++) {
+        const a = document.createElement("div");
+        document.getElementById("main").append(a);
+        a.className = "whishcontentdisplay";
+        a.id = "g" + at;
+
+        //creating for insert images on card
+        const b = document.createElement("img");
+        a.append(b);
+        b.className = "whishdisplayimage";
+        b.src = val[i].image;
+
+        const c = document.createElement("span");
+        a.append(c);
+        c.className = "whishdisplayname";
+        c.innerHTML = val[i].name;
+
+        const d = document.createElement("span");
+        a.append(d);
+        d.className = "whishdisplayname";
+        d.innerHTML =
+          "Price : " +
+          val[i].price +
+          " " +
+          '<i class="fa-solid fa-indian-rupee-sign fa-xs"></i>';
+
+        var rem = function () {
+          document.getElementById("main").innerHTML = " ";
+          let h2 = this.parentElement.id;
+          let res = h2.charAt(h2.length - 1);
+          res = Number(res);
+          val.splice(res, 1);
+          // alert(res)
+          het();
+          let whi2 = new Set(JSON.parse(localStorage.getItem("whlist")) || []);
+          val = new Set(val);
+          whi2 = val;
+          localStorage.setItem("whlist", JSON.stringify(Array.from(whi2)));
+        };
+
+        const e = document.createElement("button");
+        a.append(e);
+        e.className = "displayremovebutton";
+        e.innerHTML = "Remove";
+        e.id = "gt" + t;
+        e.addEventListener("click", rem);
+        t = t + 1;
+
+        var cartadd = function () {
+          let h2 = this.parentElement.id;
+          let res = h2.slice(1);
+          let e = val[res].id;
+          for (var i = 0; i < games.length; i++) {
+            if (games[i].id === e) {
+              alert(games[i].name + " " + "was added in your Cart");
+              let cart = new Set(
+                JSON.parse(localStorage.getItem("cart")) || []
+              );
+              cart.add(games[i]);
+              localStorage.setItem("cart", JSON.stringify(Array.from(cart)));
+            }
+          }
+        };
+
+        const f = document.createElement("button");
+        a.append(f);
+        f.className = "displaycartbutton";
+        f.innerHTML = '<i class="fa-solid fa-cart-shopping">';
+        f.addEventListener("click", cartadd);
+        at = at + 1;
+      }
+    }
+  }
+  het();
+}
+
+function cat() {
+  let whi = JSON.parse(localStorage.getItem("cart")) || [];
+  let temp = whi.filter(
+    (obj, index, self) => index === self.findIndex((o) => o.name === obj.name)
+  );
+  function het2() {
+    let val = temp;
+    let t = 0;
+    if (val.length == 0) {
+      const an1 = document.createElement("div");
+      document.getElementById("main").append(an1);
+      an1.className = "notcontent";
+
+      const bn1 = document.createElement("img");
+      an1.append(bn1);
+      bn1.className = "notimage";
+      bn1.src = "data/images/cart_empty.png";
+    } else {
+      for (i = 0; i < val.length; i++) {
+        const a = document.createElement("div");
+        document.getElementById("main").append(a);
+        a.className = "whishcontentdisplay";
+        a.id = "con" + t;
+
+        //creating for insert images on card
+        const b = document.createElement("img");
+        a.append(b);
+        b.className = "whishdisplayimage";
+        b.src = val[i].image;
+
+        const c = document.createElement("span");
+        a.append(c);
+        c.className = "whishdisplayname";
+        c.innerHTML = val[i].name;
+
+        const d = document.createElement("span");
+        a.append(d);
+        d.className = "whishdisplayname";
+        d.innerHTML =
+          "Price : " +
+          val[i].price +
+          " " +
+          '<i class="fa-solid fa-indian-rupee-sign fa-xs"></i>';
+
+        var remove = function () {
+          document.getElementById("main").innerHTML = " ";
+          let h2 = this.parentElement.id;
+          let res = h2.charAt(h2.length - 1);
+          res = Number(res);
+          val.splice(res, 1);
+          het2();
+          let cart2 = new Set(JSON.parse(localStorage.getItem("cart")) || []);
+          val = new Set(val);
+          cart2 = val;
+          localStorage.setItem("cart", JSON.stringify(Array.from(cart2)));
+        };
+
+        const e = document.createElement("button");
+        a.append(e);
+        e.className = "displayremove2button";
+        e.innerHTML = "Remove";
+        e.id = "gt" + t;
+        e.addEventListener("click", remove);
+        t = t + 1;
+
+        var buy = function () {
+          let h2 = this.parentElement.id;
+          let res = h2.charAt(h2.length - 1);
+          res = Number(res);
+          alert("Purchasing" + " " + val[res].name + " " + "Was successfully");
+          document.getElementById("main").innerHTML = " ";
+          val.splice(res, 1);
+          het2();
+          let cart2 = new Set(JSON.parse(localStorage.getItem("cart")) || []);
+          val = new Set(val);
+          cart2 = val;
+          localStorage.setItem("cart", JSON.stringify(Array.from(cart2)));
+        };
+
+        const f = document.createElement("button");
+        a.append(f);
+        f.className = "displaybuybutton";
+        f.innerHTML = '<i class="fa-solid fa-dollar-sign"></i>';
+        f.addEventListener("click", buy);
+      }
+    }
+  }
+  het2();
+}
+
+function whish(productName) {
+  // localStorage.clear();
+  // Simulate adding item to cart by storing in localStorag
+  alert(productName.name + " " + "was added in your whishlist");
+  whlist = new Set(JSON.parse(localStorage.getItem("whlist")) || []);
+  whlist.add(productName);
+  localStorage.setItem("whlist", JSON.stringify(Array.from(whlist)));
+}
+
+function car(productName) {
+  // localStorage.clear();
+  // Simulate adding item to cart by storing in localStorag
+  alert(productName.name + " " + "was added in your Cart");
+  cart = new Set(JSON.parse(localStorage.getItem("cart")) || []);
+  cart.add(productName);
+  localStorage.setItem("cart", JSON.stringify(Array.from(cart)));
+}
